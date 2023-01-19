04288b3ea43f753484bf362df2ea32d1953f5956
marynanesterenko <marynanesterenko95@gmail.com>
2023-01-18 20:02:07 -0500

    Step Definitions added

--- src/test/java/com/automation/steps/StepDefinition.java
+++ src/test/java/com/automation/steps/StepDefinition.java
@@ -31,4 +31,79 @@ public class StepDefinition {
 
     }
 
+    @Given("User open website")
+    public void user_open_website() {
+        
+    }
+
+    @Then("Verify user is on login page")
+    public void verify_user_is_on_login_page() {
+        
+    }
+
+    @When("User enter the valid credentials")
+    public void user_enter_the_valid_credentials() {
+        
+    }
+
+    @Then("Verify home page is displayed")
+    public void verify_home_page_is_displayed() {
+        
+    }
+
+    @When("User search with TV")
+    public void user_search_with_tv() {
+        
+    }
+
+    @Then("Verify user is on product listing page")
+    public void verify_user_is_on_product_listing_page() {
+        
+    }
+
+    @When("User click on the first product from the list")
+    public void user_click_on_the_first_product_from_the_list() {
+        
+    }
+
+    @Then("Verify user is on product detail page")
+    public void verify_user_is_on_product_detail_page() {
+        
+    }
+
+    @When("User click on add to cart button")
+    public void user_click_on_add_to_cart_button() {
+        
+    }
+
+    @Then("Verify cart page is displayed")
+    public void verify_cart_page_is_displayed() {
+        
+    }
+
+    @Then("Verify item on the cart page")
+    public void verify_item_on_the_cart_page() {
+        
+    }
+
+    @When("User remove item from the cart")
+    public void user_remove_item_from_the_cart() {
+        
+    }
+
+    @Then("Verify no item present in the cart")
+    public void verify_no_item_present_in_the_cart() {
+        
+    }
+
+    @When("User change quantity of the item")
+    public void user_change_quantity_of_the_item() {
+        
+    }
+
+    @Then("Verify total quantity on the cart")
+    public void verify_total_quantity_on_the_cart() {
+        
+    }
+
 }
